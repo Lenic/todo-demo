c1ef18cd66fa5788a19ddf2ae4a410d51e273d32
Lenic Rayleigh <Lenic@live.cn>
2024-09-27 21:15:20 +0800

    add prettier.
    - update the project config.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { App } from './App'
+import { App } from './App';
 
 import './index.css';
 
@@ -9,4 +9,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
