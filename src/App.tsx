00721dae13be63396b62200fb847bd485e8b2061
Lenic Rayleigh <Lenic@live.cn>
2024-10-07 16:34:34 +0800

    adjust the theme button position on cell phone.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <ThemeProvider>
       <div className="container fixed inset-0 mx-auto">
-        <div className="header pt-4 pr-4 text-right">
+        <div className="header pt-4 px-4 text-right max-md:text-center">
           <ModeToggle />
         </div>
         <div className="content md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/60 max-w-lg p-[0.375rem] rounded-lg md:shadow-2xl">
