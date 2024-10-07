6b48a7a9e74cf5b50dde45cd112ceba4bf360e4a
Lenic Rayleigh <Lenic@live.cn>
2024-10-07 15:54:16 +0800

    fix the dialog diaplay problem on the cell phone.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import { CreateNewTask, TodoPanel } from './todo';
 export const App = () => {
   return (
     <ThemeProvider>
-      <div className="container w-screen h-screen relative mx-auto">
+      <div className="container w-full h-full relative mx-auto">
         <div className="header pt-4 pr-4 text-right">
           <ModeToggle />
         </div>
-        <div className="content md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:shadow-white/60 max-w-lg p-[0.375rem] rounded-lg shadow-2xl">
+        <div className="content md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/60 max-w-lg p-[0.375rem] rounded-lg md:shadow-2xl">
           <CreateNewTask />
           <TodoPanel />
         </div>
