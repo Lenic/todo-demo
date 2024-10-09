e198e0f9900b1d1fc14d688b600a20d41cf4a061
Lenic Rayleigh <Lenic@live.cn>
2024-10-09 17:49:21 +0800

    update the background style.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ const App = () => {
   return (
     <ThemeProvider>
       <div className="container fixed inset-0 mx-auto">
-        <div className="header flex justify-end pt-4 px-4 space-x-2 max-md:justify-center">
+        <div className="header flex justify-end pt-4 px-4 space-x-2 max-md:justify-center max-md:bg-background">
           <LanguageToggle />
           <ModeToggle />
         </div>
-        <div className="content md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/60 max-w-lg p-[0.375rem] rounded-lg md:shadow-2xl">
+        <div className="content bg-background md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/20 max-w-lg p-[0.375rem] rounded-lg md:shadow-2xl max-md:rounded-none">
           <CreateNewTask />
           <TodoPanel />
         </div>
