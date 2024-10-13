fa64ec2a8696f6ad405395a425208cad1989ccd2
Lenic Rayleigh <Lenic@live.cn>
2024-10-13 23:23:19 +0800

    fix the display style problem when window width less than 768px.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
           <LanguageToggle />
           <ModeToggle />
         </div>
-        <div className="content bg-background md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/20 max-w-lg p-[0.375rem] rounded-lg md:shadow-2xl max-md:rounded-none">
+        <div className="content bg-background md:min-h-64 md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/20 max-w-lg max-md:max-w-full p-[0.375rem] rounded-lg md:shadow-2xl max-md:rounded-none">
           <CreateNewTask />
           <TodoPanel />
         </div>
