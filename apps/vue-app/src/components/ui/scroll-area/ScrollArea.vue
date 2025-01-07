c740347bfd54dfd18a302720f657b744d8dcacd1
Lenic Rayleigh <Lenic@live.cn>
2025-01-07 20:52:38 +0800

    format the code of the scroll-area component.
    - add `viewportElement$` variable.
    
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/vue-app/src/components/ui/scroll-area/ScrollArea.vue
+++ apps/vue-app/src/components/ui/scroll-area/ScrollArea.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-import {
-  ScrollAreaCorner,
-  ScrollAreaRoot,
-  type ScrollAreaRootProps,
-  ScrollAreaViewport,
-} from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
-import ScrollBar from './ScrollBar.vue'
+import type { ScrollAreaRootProps } from 'radix-vue';
+import type { HTMLAttributes } from 'vue';
 
-const props = defineProps<ScrollAreaRootProps & { class?: HTMLAttributes['class'] }>()
+import { ScrollAreaCorner, ScrollAreaRoot, ScrollAreaViewport } from 'radix-vue';
+import { filter, map } from 'rxjs';
+import { computed, ref } from 'vue';
+
+import { useObservableWatch } from '@/hooks';
+import { cn } from '@/lib/utils';
+
+import ScrollBar from './ScrollBar.vue';
+
+const props = defineProps<ScrollAreaRootProps & { class?: HTMLAttributes['class'] }>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
+
+const viewportRef = ref<{ viewportElement: HTMLDivElement }>();
+const viewport$ = useObservableWatch(viewportRef);
+
+defineExpose({
+  viewportElement$: viewport$.pipe(
+    filter((v) => !!v),
+    map((v) => v.viewportElement),
+  ),
+});
 </script>
 
 <template>
   <ScrollAreaRoot v-bind="delegatedProps" :class="cn('relative overflow-hidden', props.class)">
-    <ScrollAreaViewport class="h-full w-full rounded-[inherit]">
+    <ScrollAreaViewport ref="viewportRef" class="h-full w-full rounded-[inherit]">
       <slot />
     </ScrollAreaViewport>
     <ScrollBar />
--- apps/vue-app/src/components/ui/scroll-area/ScrollBar.vue
+++ apps/vue-app/src/components/ui/scroll-area/ScrollBar.vue
@@ -1,29 +1,34 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
-import { ScrollAreaScrollbar, type ScrollAreaScrollbarProps, ScrollAreaThumb } from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+import type { ScrollAreaScrollbarProps } from 'radix-vue';
+import type { HTMLAttributes } from 'vue';
+
+import { ScrollAreaScrollbar, ScrollAreaThumb } from 'radix-vue';
+import { computed } from 'vue';
+
+import { cn } from '@/lib/utils';
 
 const props = withDefaults(defineProps<ScrollAreaScrollbarProps & { class?: HTMLAttributes['class'] }>(), {
   orientation: 'vertical',
-})
+});
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
 </script>
 
 <template>
   <ScrollAreaScrollbar
     v-bind="delegatedProps"
     :class="
-      cn('flex touch-none select-none transition-colors',
-         orientation === 'vertical'
-           && 'h-full w-2.5 border-l border-l-transparent p-px',
-         orientation === 'horizontal'
-           && 'h-2.5 flex-col border-t border-t-transparent p-px',
-         props.class)"
+      cn(
+        'flex touch-none select-none transition-colors',
+        orientation === 'vertical' && 'h-full w-2.5 border-l border-l-transparent p-px',
+        orientation === 'horizontal' && 'h-2.5 flex-col border-t border-t-transparent p-px',
+        props.class,
+      )
+    "
   >
     <ScrollAreaThumb class="relative flex-1 rounded-full bg-border" />
   </ScrollAreaScrollbar>
