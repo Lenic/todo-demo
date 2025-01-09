6829d9cd6fa2e3daca2948cba11af83d978687e6
Lenic Rayleigh <Lenic@live.cn>
2025-01-09 21:23:01 +0800

    improve the code of the create-new component.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/vue-app/src/modules/todo/create-new.tsx
+++ apps/vue-app/src/modules/todo/create-new.tsx
@@ -4,16 +4,16 @@ import { ServiceLocator } from '@todo/container';
 import { IDataService } from '@todo/controllers';
 import { toTypedSchema } from '@vee-validate/zod';
 import { Loader2 } from 'lucide-vue-next';
-import { filter, Subject, switchMap, take, tap } from 'rxjs';
+import { filter, switchMap, take, tap } from 'rxjs';
 import { useForm } from 'vee-validate';
-import { defineComponent, onUpdated } from 'vue';
+import { defineComponent } from 'vue';
 import * as z from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { toast } from '@/components/ui/toast';
-import { useLoading, useRef } from '@/hooks';
+import { useLoading, useRef, useUpdate } from '@/hooks';
 import { useIntl } from '@/i18n';
 
 import { DatePicker } from './components/date-picker';
@@ -46,11 +46,7 @@ export const CreateNewTask = defineComponent({
       initialValues: { title: '', date: null },
     });
 
-    const updatedStatusTrigger = new Subject<void>();
-    onUpdated(() => {
-      updatedStatusTrigger.next();
-    });
-
+    const update$ = useUpdate();
     const [inputRef, input$] = useRef<{ el$: Observable<HTMLInputElement> }>();
     const [loadingRef, handleEvent] = useLoading((data: z.infer<typeof formSchema>) => {
       return dataService.add({ title: data.title, overdueAt: data.date?.valueOf() }).pipe(
@@ -58,7 +54,7 @@ export const CreateNewTask = defineComponent({
           toast({ title: t('create-success'), duration: 1_000 });
           form.resetForm();
 
-          updatedStatusTrigger
+          update$
             .pipe(
               switchMap(() => input$),
               filter((v) => !!v),
@@ -101,7 +97,7 @@ export const CreateNewTask = defineComponent({
           )}
         </FormField>
         <Button size="lg" type="submit" class="flex-initial px-4" disabled={loadingRef.value || !form.meta.value.valid}>
-          {loadingRef.value && <Loader2 class="animate-spin mr-2" width={18} height={18} />}
+          {loadingRef.value && <Loader2 class="animate-spin" width={18} height={18} />}
           {t('submit-form')}
         </Button>
       </form>
