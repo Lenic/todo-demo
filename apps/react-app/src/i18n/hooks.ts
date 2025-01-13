9d8c40957b9dcfb14c1874fa24abd9704a74984a
Lenic Rayleigh <Lenic@live.cn>
2025-01-13 23:00:25 +0800

    add a tip for the action of removing a todo item.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/react-app/src/i18n/hooks.ts
+++ apps/react-app/src/i18n/hooks.ts
@@ -4,7 +4,10 @@ import { useTranslation } from 'react-i18next';
 export const useIntl = (prefix: string) => {
   const { t, i18n } = useTranslation();
 
-  const handleTranslate = useCallback((key: string) => t(`${prefix}.${key}`), [prefix, t]);
+  const handleTranslate = useCallback(
+    (key: string, literal: Record<string, unknown> = {}) => t(`${prefix}.${key}`, literal),
+    [prefix, t],
+  );
 
   return { t: handleTranslate, i18n };
 };
--- apps/react-app/src/modules/todo/components/row-dropdown-menu.tsx
+++ apps/react-app/src/modules/todo/components/row-dropdown-menu.tsx
@@ -1,16 +1,28 @@
+import type { FC, ReactNode } from 'react';
+
 import { ServiceLocator } from '@todo/container';
 import { IDataService } from '@todo/controllers';
-import { FilePenLine, Trash2 } from 'lucide-react';
-import { type FC, type ReactNode, useCallback, useEffect, useRef, useState } from 'react';
+import { FilePenLine, Loader2, Trash2 } from 'lucide-react';
+import { useCallback, useMemo, useState } from 'react';
+import { filter, finalize, map } from 'rxjs';
 
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { useLoading } from '@/hooks';
-import { useToast } from '@/hooks/use-toast';
+import { useLoading, useObservableState } from '@/hooks';
 import { useIntl } from '@/i18n';
 
 export interface IRowDropdownMenuProps {
@@ -23,43 +35,64 @@ const dataService = ServiceLocator.default.get(IDataService);
 
 export const RowDropdownMenu: FC<IRowDropdownMenuProps> = ({ id, children, onDetail }) => {
   const { t } = useIntl('todo.item-context-menu');
-  const [, handleRemove, loading$] = useLoading(() => dataService.delete(id));
 
-  const idRef = useRef('');
-  const { toast, dismiss } = useToast();
-  useEffect(() => {
-    const subscription = loading$.subscribe((loading) => {
-      if (loading) {
-        idRef.current = toast({ title: t('executing'), duration: 0 }).id;
-      } else if (idRef.current) {
-        dismiss(idRef.current);
-        idRef.current = '';
-      }
-    });
+  const title = useObservableState(
+    useMemo(
+      () =>
+        dataService.dataMapper$.pipe(
+          map((mapper) => mapper[id]),
+          filter((v) => !!v),
+          map((item) => item.title),
+        ),
+      [id],
+    ),
+    '',
+  );
 
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [loading$, t, dismiss, toast]);
+  const [showRemoveDialog, setShowRemoveDialog] = useState(false);
+  const handleOpenDialog = useCallback(() => {
+    setShowRemoveDialog(true);
+  }, []);
+  const handleCloseDialog = useCallback(() => {
+    setShowRemoveDialog(false);
+  }, []);
+  const [loading, handleRemove] = useLoading(() => dataService.delete(id).pipe(finalize(handleCloseDialog)));
 
   const handleModify = useCallback(() => {
     onDetail?.(id);
-  }, [id, onDetail]);
+  }, [onDetail, id]);
 
   const [open, setOpen] = useState(false);
   return (
-    <DropdownMenu open={open} onOpenChange={setOpen}>
-      <DropdownMenuTrigger className="leading-5 h-5">{children(open)}</DropdownMenuTrigger>
-      <DropdownMenuContent align="start" side="bottom">
-        <DropdownMenuItem onClick={handleModify}>
-          <FilePenLine className="h-4 w-4 mr-2" />
-          {t('edit')}
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={handleRemove}>
-          <Trash2 className="h-4 w-4 mr-2" />
-          {t('remove')}
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger className="leading-5 h-5">{children(open)}</DropdownMenuTrigger>
+        <DropdownMenuContent align="start" side="bottom">
+          <DropdownMenuItem onClick={handleModify}>
+            <FilePenLine className="h-4 w-4 mr-2" />
+            {t('edit')}
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleOpenDialog}>
+            <Trash2 className="h-4 w-4 mr-2" />
+            {t('remove')}
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialog open={showRemoveDialog} onOpenChange={setShowRemoveDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{t('remove-dialog.title')}</AlertDialogTitle>
+            <AlertDialogDescription>{t('remove-dialog.description', { val: title })}</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={loading}>{t('remove-dialog.cancel')}</AlertDialogCancel>
+            <Button variant="destructive" disabled={loading} onClick={handleRemove}>
+              {loading && <Loader2 className="animate-spin mr-2" width={18} height={18} />}
+              {t('remove')}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
