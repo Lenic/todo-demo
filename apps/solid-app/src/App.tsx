55f9677b354aad88f5088205008094c59c34bba3
Lenic Rayleigh <Lenic@live.cn>
2025-01-15 13:18:12 +0800

    initialize the app component.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/solid-app/src/App.tsx
+++ apps/solid-app/src/App.tsx
@@ -1,21 +1,14 @@
-import { createSignal } from 'solid-js';
-
-import { Button } from '@/components/ui/button';
-
-function App() {
-  const [count, setCount] = createSignal(0);
-
+const App = () => {
   return (
-    <>
-      <h1>Vite + Solid</h1>
-      <div>
-        <Button onClick={() => setCount((count) => count + 1)}>current count is {count()}</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div class="container fixed inset-0 mx-auto">
+      <div class="header flex justify-end pt-4 px-4 space-x-2 max-md:justify-center max-md:bg-background">
+        <div>abc</div>
       </div>
-    </>
+      <div class="content bg-background md:min-h-64 md:w-[450px] md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/20 max-w-lg max-md:max-w-full p-[0.375rem] rounded-lg md:shadow-2xl max-md:rounded-none">
+        <div>xyz</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
