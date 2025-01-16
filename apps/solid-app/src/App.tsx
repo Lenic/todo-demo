cdbde47607684b8bef302021e0613b3bc3f2682c
Lenic Rayleigh <Lenic@live.cn>
2025-01-16 20:36:45 +0800

    add the todo-panel component.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/solid-app/src/App.tsx
+++ apps/solid-app/src/App.tsx
@@ -5,6 +5,7 @@ import { useIntl } from '@/i18n';
 import { LanguageToggle } from '@/modules/setting/language-toggle';
 import { ThemeColorToggle } from '@/modules/setting/theme-color-toggle';
 import { ThemeToggle } from '@/modules/setting/theme-toggle';
+import { TodoPanel } from '@/modules/todo';
 
 const App = () => {
   ServiceLocator.default.get(IThemeService).initialize();
@@ -18,7 +19,7 @@ const App = () => {
         <ThemeColorToggle />
       </div>
       <div class="content bg-background md:min-h-64 md:w-[450px] md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 dark:md:shadow-white/20 max-w-lg max-md:max-w-full p-[0.375rem] rounded-lg md:shadow-2xl max-md:rounded-none">
-        <div>{t('description', { val: 'abc' })}</div>
+        <TodoPanel />
       </div>
     </div>
   );
---
+++ apps/solid-app/src/modules/todo/panel.tsx
@@ -0,0 +1,31 @@
+import { ETodoListType } from '@todo/controllers';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useIntl } from '@/i18n';
+
+// import { TodoList } from './list';
+
+export const TodoPanel = () => {
+  const { t } = useIntl('todo.panel');
+
+  return (
+    <Tabs defaultValue={ETodoListType.PENDING} class="w-full">
+      <TabsList class="w-full grid grid-cols-3">
+        <TabsTrigger value={ETodoListType.PENDING}>{t('pending')}</TabsTrigger>
+        <TabsTrigger value={ETodoListType.OVERDUE}>{t('overdue')}</TabsTrigger>
+        <TabsTrigger value={ETodoListType.ARCHIVE}>{t('archive')}</TabsTrigger>
+      </TabsList>
+      <TabsContent value="PENDING" class="pl-2">
+        <div>PENDING</div>
+      </TabsContent>
+      <TabsContent value="OVERDUE" class="pl-2">
+        <div>OVERDUE</div>
+      </TabsContent>
+      <TabsContent value="ARCHIVE" class="pl-2">
+        <div>ARCHIVE</div>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+// <TodoList type={ETodoListType.PENDING} />
