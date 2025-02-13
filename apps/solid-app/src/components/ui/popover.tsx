efa9cb1a9497bd28fd8e3d99d0a6bfafc73cc3c5
Lenic Rayleigh <Lenic@live.cn>
2025-02-13 21:49:56 +0800

    remove the close button from the popover component.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/solid-app/src/components/ui/popover.tsx
+++ apps/solid-app/src/components/ui/popover.tsx
@@ -44,19 +44,6 @@ export const PopoverContent = <T extends ValidComponent = 'div'>(
         {...rest}
       >
         {local.children}
-        <PopoverPrimitive.CloseButton class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-[opacity,box-shadow] hover:opacity-100 focus:outline-none focus:ring-[1.5px] focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4">
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M18 6L6 18M6 6l12 12"
-            />
-            <title>Close</title>
-          </svg>
-        </PopoverPrimitive.CloseButton>
       </PopoverPrimitive.Content>
     </PopoverPrimitive.Portal>
   );
