45c85d3d338ece32719319f8221dc1f478a92292
Lenic Rayleigh <Lenic@live.cn>
2025-03-20 18:51:36 +0800

    set the eslint configuration.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

---
+++ apps/nextjs-app/eslint.config.js
@@ -0,0 +1,54 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import { dirname } from 'path';
+import lint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import globalSettings from '../../eslint.config.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...globalSettings,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: lint.parser,
+        projectService: true,
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  // ...compat.extends('next/core-web-vitals', 'next/typescript'),
+];
