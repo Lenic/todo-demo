01562dd2660dc31fa56d0e7d678d83f3c6d0b61f
Lenic Rayleigh <Lenic@live.cn>
2025-03-21 21:45:51 +0800

    fix the trpc context definition.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/nextjs-app/trpc/server.ts
+++ apps/nextjs-app/trpc/server.ts
@@ -1,10 +1,19 @@
+import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
+
 import { initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 
-const t = initTRPC.create({
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
+  return {
+    req: opts.req,
+    res: opts.res,
+  };
+};
+export type TRPCContext = Awaited<ReturnType<typeof createTRPCContext>>;
+
+const t = initTRPC.context<TRPCContext>().create({
   transformer: superjson,
 });
 
-export const createTRPCContext = t.context;
 export const router = t.router;
 export const publicProcedure = t.procedure;
