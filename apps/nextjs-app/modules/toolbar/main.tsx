bdd2b369d9110b71b6ce455a638aa3ea2e471114
Lenic Rayleigh <Lenic@live.cn>
2025-04-07 16:51:56 +0800

    remove the test code.

--- apps/nextjs-app/modules/toolbar/main.tsx
+++ apps/nextjs-app/modules/toolbar/main.tsx
@@ -3,9 +3,6 @@
 import { ServiceLocator } from '@todo/container';
 import { IThemeService } from '@todo/controllers';
 import { useEffect } from 'react';
-import { toast } from 'sonner';
-
-import { Button } from '@/components/ui/button';
 
 import { LanguageToggle } from './components/language-toggle';
 import { ThemeColorToggle } from './components/theme-color-toggle';
@@ -18,22 +15,6 @@ export function Toolbar() {
 
   return (
     <div className="header flex justify-end pt-4 px-4 space-x-2 max-md:justify-center max-md:bg-background">
-      <Button
-        variant="outline"
-        onClick={() =>
-          toast('Event has been created', {
-            description: 'Sunday, December 03, 2023 at 9:00 AM',
-            action: {
-              label: 'Undo',
-              onClick: () => {
-                console.log('Undo');
-              },
-            },
-          })
-        }
-      >
-        Show Toast
-      </Button>
       <LanguageToggle />
       <ThemeToggle />
       <ThemeColorToggle />
