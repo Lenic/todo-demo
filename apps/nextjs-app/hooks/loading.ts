433177f3555a0bb512fe7e335eb3b0426f3c1cd1
Lenic Rayleigh <Lenic@live.cn>
2025-04-07 16:13:44 +0800

    immigrate the common hooks from react-app.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

---
+++ apps/nextjs-app/hooks/loading.ts
@@ -0,0 +1,67 @@
+import type { Observable } from 'rxjs';
+
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  catchError,
+  exhaustMap,
+  from,
+  map,
+  merge,
+  of,
+  race,
+  ReplaySubject,
+  share,
+  Subject,
+  take,
+  timer,
+  withLatestFrom,
+} from 'rxjs';
+
+export const useLoading = <T>(fn: (args: T) => Promise<unknown> | Observable<unknown>, delayTime = 300) => {
+  const eventArgsRef = useRef(new Subject<T>());
+  const handleEvent = useCallback((args: T) => {
+    eventArgsRef.current.next(args);
+  }, []);
+
+  const actionRef = useRef(new ReplaySubject<(args: T) => Promise<unknown> | Observable<unknown>>(1));
+  useEffect(() => {
+    actionRef.current.next(fn);
+  }, [fn]);
+
+  const [loading, setLoading] = useState(false);
+  const [loadingTrigger] = useState(() => new Subject<boolean>());
+  useEffect(() => {
+    const subscription = loadingTrigger.subscribe(setLoading);
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [loadingTrigger]);
+
+  useEffect(() => {
+    const subscription = eventArgsRef.current
+      .pipe(
+        withLatestFrom(actionRef.current),
+        exhaustMap(([eventArgs, action]) => {
+          const waiter = action(eventArgs);
+          const waiter$ = waiter instanceof Promise ? from(waiter) : waiter.pipe(take(1));
+
+          const false$ = waiter$.pipe(
+            map(() => false),
+            catchError(() => of(false)),
+            share(),
+          );
+
+          return merge(false$, race(false$, timer(delayTime).pipe(map(() => true))));
+        }),
+      )
+      .subscribe((loading) => {
+        loadingTrigger.next(loading);
+      });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [delayTime, loadingTrigger]);
+
+  return [loading, handleEvent, loadingTrigger] as const;
+};
---
+++ apps/nextjs-app/hooks/observable-state.ts
@@ -0,0 +1,29 @@
+import type { Observable } from 'rxjs';
+
+import { useEffect, useRef, useState } from 'react';
+
+export const defaultEqualComparer = <T>(prev: T, next: T) => prev === next;
+
+export function useObservableState<T>(
+  source$: Observable<T>,
+  valueOrGetter: T | (() => T),
+  comparer: (prev: T, next: T) => boolean = defaultEqualComparer,
+) {
+  const [state, setState] = useState(valueOrGetter);
+  const currentValueRef = useRef(state);
+
+  useEffect(() => {
+    const subscription = source$.subscribe((value) => {
+      if (comparer(currentValueRef.current, value)) return;
+
+      currentValueRef.current = value;
+      setState(value);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [source$, comparer]);
+
+  return state;
+}
