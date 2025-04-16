1b0f0c3916461072e566ca1b08e0ef1fc3163933
Lenic Rayleigh <Lenic@live.cn>
2025-04-16 20:49:51 +0800

    enhance the code formatting.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/nextjs-app/components/ui/form.tsx
+++ apps/nextjs-app/components/ui/form.tsx
@@ -102,7 +102,7 @@ function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
     <Slot
       data-slot="form-control"
       id={formItemId}
-      aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
+      aria-describedby={!error ? formDescriptionId : `${formDescriptionId} ${formMessageId}`}
       aria-invalid={!!error}
       {...props}
     />
@@ -124,7 +124,7 @@ function FormDescription({ className, ...props }: React.ComponentProps<'p'>) {
 
 function FormMessage({ className, ...props }: React.ComponentProps<'p'>) {
   const { error, formMessageId } = useFormField();
-  const body = error ? String(error?.message ?? '') : props.children;
+  const body = error ? String(error.message ?? '') : props.children;
 
   if (!body) {
     return null;
