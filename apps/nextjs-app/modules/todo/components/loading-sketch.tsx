ef4ae6d071ab74b4e76cb0e1200666c71cb064d6
Lenic Rayleigh <Lenic@live.cn>
2025-04-24 15:09:35 +0800

    fix the hydration exception of the loading-sketch component.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/nextjs-app/modules/todo/components/loading-sketch.tsx
+++ apps/nextjs-app/modules/todo/components/loading-sketch.tsx
@@ -5,8 +5,8 @@ import type { FC } from 'react';
 
 import { ServiceLocator } from '@todo/container';
 import { areArraysEqual, IDataService } from '@todo/interface';
-import dynamic from 'next/dynamic';
-import { memo, useMemo, useRef } from 'react';
+import { memo, useEffect, useMemo, useRef, useState } from 'react';
+import ContentLoader from 'react-content-loader';
 import { of } from 'rxjs';
 import { delay, distinctUntilChanged, map, pairwise, startWith, switchMap } from 'rxjs/operators';
 
@@ -16,11 +16,6 @@ import { getElementResize$ } from '@/lib/utils';
 export interface ILoadingSketchProps {
   type: ETodoListType;
 }
-
-const ContentLoader = dynamic(
-  () => import('react-content-loader').then((v) => v.default),
-  { ssr: false }, // 关键配置
-);
 
 const defaultRowWidth = [364, 300, 332] as [number, number, number];
 
@@ -62,20 +57,27 @@ const LoadingSketchCore: FC<ILoadingSketchProps> = ({ type }) => {
     areArraysEqual,
   );
 
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div ref={containerRef}>
-      <ContentLoader
-        speed={0.5}
-        width={rowWidth[0]}
-        height={40}
-        viewBox={`0 0 ${String(rowWidth[0])} 40`}
-        backgroundColor="#d9d9d9"
-        foregroundColor="#ededed"
-      >
-        <rect x="0" y="12" rx="1" ry="1" width="16" height="16" />
-        <rect x="24" y="10" rx="4" ry="4" width={rowWidth[1]} height="20" />
-        <rect x={rowWidth[2]} y="12" rx="1" ry="1" width="16" height="16" />
-      </ContentLoader>
+      {isClient && (
+        <ContentLoader
+          speed={0.5}
+          width={rowWidth[0]}
+          height={40}
+          viewBox={`0 0 ${String(rowWidth[0])} 40`}
+          backgroundColor="#d9d9d9"
+          foregroundColor="#ededed"
+        >
+          <rect x="0" y="12" rx="1" ry="1" width="16" height="16" />
+          <rect x="24" y="10" rx="4" ry="4" width={rowWidth[1]} height="20" />
+          <rect x={rowWidth[2]} y="12" rx="1" ry="1" width="16" height="16" />
+        </ContentLoader>
+      )}
     </div>
   );
 };
