212c5868fade602159f3306e4d242c9cbe5ec0dd
Lenic Rayleigh <Lenic@live.cn>
2025-04-24 15:59:11 +0800

    fix the hydration exception of the scroll-area component.
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- apps/nextjs-app/components/ui/scroll-area.tsx
+++ apps/nextjs-app/components/ui/scroll-area.tsx
@@ -3,6 +3,7 @@
 import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
 import * as React from 'react';
 
+import { useClient } from '@/hooks';
 import { cn } from '@/lib/utils';
 
 function ScrollArea({ className, children, ...props }: React.ComponentProps<typeof ScrollAreaPrimitive.Root>) {
@@ -25,7 +26,11 @@ function ScrollBar({
   orientation = 'vertical',
   ...props
 }: React.ComponentProps<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>) {
-  return (
+  const isClient = useClient();
+
+  return isClient ? (
+    props.children
+  ) : (
     <ScrollAreaPrimitive.ScrollAreaScrollbar
       data-slot="scroll-area-scrollbar"
       orientation={orientation}
