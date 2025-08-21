4bf182de3a42490b0cfd7d9d751d519be6228548
Lenic Lei <lenic_lei@manka.co.jp>
2025-08-21 17:34:48 +0900

    initialize the nuxt.js project.
    Signed-off-by: Lenic Lei <lenic_lei@manka.co.jp>

---
+++ apps/nuxtjs-app/app/app.tsx
@@ -0,0 +1,15 @@
+import { defineComponent } from 'vue';
+import { Button } from './components/ui/button';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => (
+      <div>
+        <div>Route Announcer</div>
+        <div>Welcome to Nuxt</div>
+        <Button>Click me</Button>
+      </div>
+    );
+  },
+});
---
+++ apps/nuxtjs-app/nuxt.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+})
